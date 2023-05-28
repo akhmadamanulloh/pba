8874423ed316c060b7c171b9ed5ed7a258e5a643
Akhmad Amanulloh <84214955+akhmadamanulloh@users.noreply.github.com>
2023-05-29 06:26:23 +0700

    Update pba_streamlit.py

--- pba_streamlit.py
+++ pba_streamlit.py
@@ -10,6 +10,7 @@ from textblob import TextBlob
 import streamlit as st
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
+pip install sastrawi
 
 nltk.download('punkt')
 nltk.download('stopwords')
