466aafd073d07182dfc8cfe449046843620385cc
Akhmad Amanulloh <84214955+akhmadamanulloh@users.noreply.github.com>
2023-05-29 06:25:12 +0700

    Update pba_streamlit.py

--- pba_streamlit.py
+++ pba_streamlit.py
@@ -1,123 +1,87 @@
-import streamlit as st
 import pandas as pd
+import re
+import string
 import pickle
 import nltk
-from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
-from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from textblob import TextBlob
-from sklearn.feature_extraction.text import TfidfVectorizer
+import streamlit as st
+from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
-import os
 
 nltk.download('punkt')
 nltk.download('stopwords')
 
-# Download stopword Bahasa Indonesia
-nltk.download('stopwords')
-stop_words_id = set(stopwords.words('indonesian'))
-
-# Fungsi untuk melakukan preprocessing teks
+# Fungsi untuk melakukan preprosesing teks
 def preprocess_text(text):
     # Case folding
     text = text.lower()
-
-    # Filtering dan tokenizing
+    
+    # Filtering
+    text = re.sub('\[.*?\]', '', text)
+    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('\w*\d\w*', '', text)
+    
+    # Tokenizing
     tokens = word_tokenize(text)
-    tokens = [token for token in tokens if token.isalpha() and token not in stop_words_id]
-
+    
     # Stemming
-    stemmer = PorterStemmer()
-    tokens = [stemmer.stem(token) for token in tokens]
+    factory = StemmerFactory()
+    stemmer = factory.create_stemmer()
+    stemmed_tokens = [stemmer.stem(token) for token in tokens]
+    
+    # Menghapus stop words
+    stop_words = set(stopwords.words("english")) | set(stopwords.words("indonesian"))
+    filtered_tokens = [token for token in stemmed_tokens if token not in stop_words]
+    
+    # Menggabungkan kembali kata-kata yang telah diproses
+    preprocessed_text = ' '.join(filtered_tokens)
+    
+    return preprocessed_text
 
-    return ' '.join(tokens)
+# Membaca data CSV dan menyimpan ke dalam DataFrame
+df = pd.read_csv('data_tweet.csv')
 
-# Fungsi untuk mendapatkan sentimen menggunakan TextBlob
-def get_sentiment(text):
-    blob = TextBlob(text)
-    sentiment = blob.sentiment.polarity
-    if sentiment > 0:
-        return 'Positif'
-    elif sentiment < 0:
-        return 'Negatif'
+# Melakukan analisis sentimen pada setiap tweet
+def get_sentiment(tweet):
+    # Menghitung polaritas tweet
+    polarity = TextBlob(tweet).sentiment.polarity
+    
+    # Menentukan sentimen tweet berdasarkan nilai polaritas
+    if polarity > 0:
+        return '1'
+    elif polarity < 0:
+        return '-1'
     else:
-        return 'Netral'
+        return '0'
 
-# Fungsi untuk melatih model Naive Bayes dan menyimpannya ke dalam file pickle
-def train_model(df):
-    # Preprocessing teks
-    df['Preprocessed_Text'] = df['Tweet'].apply(preprocess_text)
-    df['Sentiment'] = df['Preprocessed_Text'].apply(get_sentiment)
+df['sentiment'] = df['Tweet'].apply(get_sentiment)
 
-    # Memisahkan fitur dan label
-    X = df['Preprocessed_Text']
-    y = df['Sentiment']
+# Melakukan preprosesing pada teks ulasan
+df['preprocessed_text'] = df['Tweet'].apply(preprocess_text)
 
-    # Melakukan vektorisasi teks menggunakan TF-IDF
-    vectorizer = TfidfVectorizer()
-    X = vectorizer.fit_transform(X)
+# Melakukan analisis sentimen menggunakan Naive Bayes
+vectorizer = CountVectorizer()
+X = vectorizer.fit_transform(df['preprocessed_text'])
+y = df['sentiment']
 
-    # Melatih model Naive Bayes
-    model = MultinomialNB()
-    model.fit(X, y)
+model = MultinomialNB()
+model.fit(X, y)
 
-    # Menyimpan model ke dalam file pickle
-    with open('sentiment_model.pkl', 'wb') as f:
-        pickle.dump(model, f)
+# Simpan model dalam file pickle
+with open('sentiment_model.pickle', 'wb') as file:
+    pickle.dump(model, file)
 
-    # Menyimpan vectorizer ke dalam file pickle
-    with open('vectorizer.pkl', 'wb') as f:
-        pickle.dump(vectorizer, f)
-
-# Fungsi untuk memuat model dari file pickle
-def load_model():
-    try:
-        with open('sentiment_model.pkl', 'rb') as f:
-            model = pickle.load(f)
-
-        with open('vectorizer.pkl', 'rb') as f:
-            vectorizer = pickle.load(f)
-
-        return model, vectorizer
-    except FileNotFoundError:
-        return None, None
-
-# Fungsi utama Streamlit
-def main():
-    st.title('Analisis Sentimen Data Waralaba dari Tweet')
-
-    # Tombol untuk melatih model dan menyimpan ke dalam file pickle
-    if st.button('Melatih Model'):
-        df = pd.read_csv('data_tweet.csv')
-        train_model(df)
-        st.success('Model berhasil dilatih dan disimpan ke dalam file sentiment_model.pkl')
-
-    # Tombol untuk memuat model dari file pickle
-    if st.button('Memuat Model'):
-        model, vectorizer = load_model()
-        if model is not None and vectorizer is not None:
-            st.success('Model berhasil dimuat dari file sentiment_model.pkl')
-        else:
-            st.error('Model belum dilatih atau belum dimuat. Silakan klik tombol "Melatih Model" terlebih dahulu.')
-
-    # Memuat model dari file pickle
-    model, vectorizer = load_model()
-
-    # Kolom input teks untuk analisis sentimen
-    st.subheader('Analisis Sentimen')
-    review_text = st.text_input('Masukkan tweet tentang waralaba')
-
-    # Tombol untuk menganalisis sentimen
-    if st.button('Analisis'):
-        if model is not None and vectorizer is not None:
-            preprocessed_text = preprocess_text(review_text)
-            X = vectorizer.transform([preprocessed_text])
-            predicted_sentiment = model.predict(X)[0]
-            original_sentiment = get_sentiment(review_text)
-            st.write('Sentimen Asli:', original_sentiment)
-            st.write('Sentimen Prediksi:', predicted_sentiment)
-        else:
-            st.error('Model belum dilatih atau belum dimuat. Silakan klik tombol "Melatih Model" terlebih dahulu.')
-
-if __name__ == '__main__':
-    main()
+# Implementasi aplikasi Streamlit
+st.title("Analisis Sentimen Waralaba")
+text_input = st.text_input("Masukkan teks ulasan:")
+if st.button("Prediksi Sentimen"):
+    if text_input:
+        preprocessed_text = preprocess_text(text_input)
+        text_vectorized = vectorizer.transform([preprocessed_text])
+        sentiment = model.predict(text_vectorized)[0]
+        st.write("Sentimen: ", sentiment)
+    else:
+        st.write("Masukkan teks ulasan untuk melakukan prediksi sentimen.")
