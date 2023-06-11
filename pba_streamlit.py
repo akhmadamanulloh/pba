b3f84ecb0f5c47194d6eb8aae6f062fc189eba14
Akhmad Amanulloh <84214955+akhmadamanulloh@users.noreply.github.com>
2023-06-11 21:14:27 +0700

    Update pba_streamlit.py

--- pba_streamlit.py
+++ pba_streamlit.py
@@ -69,7 +69,7 @@ X = vectorizer.fit_transform(df['preprocessed_text'])
 y = df['sentiment']
 
 # Memisahkan data menjadi data latih (train) dan data uji (test)
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
 
 # Membangun model Naive Bayes
 naive_bayes_model = MultinomialNB()
