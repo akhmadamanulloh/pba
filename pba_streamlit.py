a55a5dc28fe2667dbc00a0c5016c7d182803681d
Akhmad Amanulloh <84214955+akhmadamanulloh@users.noreply.github.com>
2023-10-12 01:38:58 +0700

    Update pba_streamlit.py

--- pba_streamlit.py
+++ pba_streamlit.py
@@ -46,18 +46,30 @@ with Ekstraksi :
    tf_idf
 
 with lda:
-   lda = LatentDirichletAllocation(n_components=3, doc_topic_prior=0.2, topic_word_prior=0.1,random_state=42,max_iter=1)
-   lda_top=lda.fit_transform(tf)
-   #bobot setiap topik terhadap dokumen
-   U = pd.DataFrame(lda_top, columns=['Topik 1','Topik 2','Topik 3'])
-   U['Label']=TF['Label'].values
-   U
+   topik = st.number_input("Masukkan Jumlah Topik yang Diinginkan", 1, step=1)
+
+   def submit():
+        lda = LatentDirichletAllocation(n_components=topik, doc_topic_prior=0.2, topic_word_prior=0.1,random_state=42,max_iter=1)
+        x=tf.drop('Label', axis=1)
+        lda_top=lda.fit_transform(x)
+        #bobot setiap topik terhadap dokumen
+        nama_clm =[]
+        for i in range(topik):
+            nama_clm.append(("Topik "+ str(i+1)))
+        U = pd.DataFrame(lda_top, columns=nama_clm)
+        U['Label']=tf['Label'].values
+        U
+   all = st.button("Submit")
+   if all :
+      st.balloons()
+      submit()
 
 with Model :
     # if all :
         st.subheader('Jumlah Topik yang Anda Gunakan : ' +str(topik))
         st.write ("Jika pada menu LDA tidak menentukan jumlah topiknya maka proses modelling akan di default dengan jumlah topik = 1")
         lda = LatentDirichletAllocation(n_components=topik, doc_topic_prior=0.2, topic_word_prior=0.1,random_state=42,max_iter=1)
+        x=tf.drop(columns='Label')
         lda_top=lda.fit_transform(x)
         y = tf.Label
         X_train,X_test,y_train,y_test = train_test_split(lda_top,y,test_size=0.2,random_state=42)
